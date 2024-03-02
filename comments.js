12f640ab3b73de006c15284da082a9a342533bdb
Arturo Santiago <varturosm@gmail.com>
2024-03-02 18:10:22 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,30 +1,57 @@
-// create web server
+//create web server
+var http = require('http');
 var express = require('express');
 var app = express();
+var server = http.createServer(app);
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var path = require('path');
+var url = require('url');
 
+//add body-parser
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
-// create in-memory database
-var comments = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
+//set the port
+app.set('port', process.env.PORT || 3000);
 
-app.get('/api/comments', function(req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(comments));
+//set the path
+app.use(express.static(path.join(__dirname, 'public')));
+
+//set the path for the comments
+var commentsPath = path.join(__dirname, 'comments.json');
+
+//read the comments
+app.get('/comments', function(req, res) {
+  fs.readFile(commentsPath, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
 });
 
-app.post('/api/comments', function(req, res) {
-  var newComment = req.body;
-  newComment.id = Date.now();
-  comments.push(newComment);
-  res.setHeader('Content-Type', 'application/json');
-  res.send(JSON.stringify(newComment));
+//write the comments
+app.post('/comments', function(req, res) {
+  fs.readFile(commentsPath, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile(commentsPath, JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(comments);
+    });
+  });
 });
 
-app.listen(3000, function() {
-  console.log('Server started: http://localhost:3000/');
+//start the server
+server.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
 });
-
